29b4fcc500970410d8870783a91f1452447e4078
Suyel Bishnu <suyel2007@gmail.com>
2024-09-18 12:33:54 +0300

    Finish the react-test-module

--- web/modules/custom/react_test_module/js/react-test/src/App.jsx
+++ web/modules/custom/react_test_module/js/react-test/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [catImages, setCatImages] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.thecatapi.com/v1/images/search?limit=10")
+      .then((response) => {
+        setCatImages(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching cat images:", error);
+      });
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredImages = catImages.filter((catImage) =>
+    catImage.id.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Cat Images</h1>
+      <input
+        type="text"
+        placeholder="Search by id"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <ul>
+        {filteredImages.map((catImage) => (
+          <li key={catImage.id}>
+            <img 
+            src={catImage.url} 
+            alt={`Cat ${catImage.id}`}
+             />
+            <p>{catImage.id}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
